b1a4977e35acd2d3bbfad9dde20d36a4ed128e7a
lmedina002 <lmedina002@ensc.fr>
2021-01-31 18:54:47 +0100

    start get-move

--- rules.lisp
+++ rules.lisp
@@ -1,28 +1,48 @@
 ;;;; File containing all the functions for implementing the rules of shogi
 
 (defun init-empty-board ()
-  "Create an empty board indexed by letter and number"
+  "Create an empty board indexed by letter rows and columns"
   (list
-   :a (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
-   :b (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
-   :c (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
-   :d (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
-   :e (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
-   :f (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
-   :g (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
-   :h (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
-   :i (list :1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")))
+   :1 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
+   :2 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
+   :3 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
+   :4 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
+   :5 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
+   :6 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
+   :7 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
+   :8 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")
+   :9 '(:1 "" :2 "" :3 "" :4 "" :5 "" :6 "" :7 "" :8 "" :9 "")))
 
-(defun select-square (board letter number)
+(defun select-square (board row column)
   "Return the piece on the square"
-  (getf (getf board letter) number))
+  (getf (getf board row) column))
 
-(defun move (board letter number)
-  (move-piece (board (select-square board letter number))))
+(defun move (board row column)
+  (move-piece (board (select-square board row column))))
 
-(defun move-piece (board piece)
-  (cond ((equal piece "P") (move-pawn))
+;(defun move-piece (board piece direction)
+;  (cond ((equal piece "P") (move-pawn))
 	
-  
+(defun get-available-move (piece row column)
+  (cond ((equal piece "P") (get-move-pawn row column))
+	((equal piece "L") (get-move-lance row column))
+	((equal piece "N") (get-move-knight row column))
+	((equal piece "S") (get-move-silver row column))
+	((equal piece "G") (get-move-gold row column))
+	((equal piece "B") (get-move-bishop row column))
+	((equal piece "R") (get-move-rook row column))
+	((equal piece "K") (get-move-knight row column))
+	((equal piece "J") (get-move-jewel row column)) ;roi adverse a voir si utile
+	((equal piece "+R") (get-move-dragon row column))
+	((equal piece "+B") (get-move-horse row column))
+	((or (equal piece "+S") (equal piece "+N") (equal piece "+L") (equal piece "+P")) (get-move-gold row column))))
+
+;;; All get-move-... function that return the available moves
+(defun get-move-pawn (row column)
+  (if (> row 1) (+ row 1) (promote piece)))
+	
+	
+	
+	
   
    
